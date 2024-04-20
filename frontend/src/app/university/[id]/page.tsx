cdaf1d2990328dca70c16f56bd174d5dd710e905
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-04-20 15:52:47 +0300

    frontend: add useUniversity

--- frontend/src/app/university/[id]/page.tsx
+++ frontend/src/app/university/[id]/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { UniversityOpenDays } from "@/components/university-open-days";
 import { UniversityReviews } from "@/components/university-reviews";
+import { useUniversity } from "@/lib/use-university";
 import Image from "next/image";
 
 interface Params {
@@ -8,73 +12,71 @@ interface Params {
 }
 
 export default function Page({ params }: { params: Params }) {
-  const universityName = "ДонНТУ";
-  const universityLongName = "Донецкий национальный технический университет";
+  const { data, isLoading, error } = useUniversity(params.id);
 
   return (
     <main className="min-h-screen">
       <div className="bg-white p-6 shadow-lg rounded-lg max-w-4xl mx-auto">
         <div className="flex items-center justify-between mb-4">
-          <div className="flex items-center">
-            <Image
-              alt="Logo"
-              className="h-12 w-12 mr-3"
-              height="50"
-              src="https://donntu.ru/sites/all/themes/donntu/logo.png"
-              style={{
-                aspectRatio: "50/50",
-                objectFit: "contain",
-              }}
-              width="50"
-            />
-            <div>
-              <h1 className="text-xl font-bold">{universityName}</h1>
-              <h2 className="text-lg">{universityLongName}</h2>
-              <div className="flex items-center mt-1"></div>
-            </div>
+          <div className="flex items-center w-full">
+            {isLoading ? (
+              <Skeleton className="h-12 w-12 mr-3" />
+            ) : (
+              <img
+                alt="Logo"
+                className="h-12 w-12 mr-3"
+                height="50"
+                src={data?.logoUrl}
+                style={{
+                  aspectRatio: "50/50",
+                  objectFit: "contain",
+                }}
+                width="50"
+              />
+            )}
+            {isLoading ? (
+              <div className="w-full">
+                <Skeleton className="h-6 w-1/4" />
+                <Skeleton className="h-6 w-1/2 mt-2" />
+              </div>
+            ) : (
+              <div>
+                <h1 className="text-xl font-bold">{data?.name}</h1>
+                <h2 className="text-lg">{data?.longName}</h2>
+              </div>
+            )}
           </div>
-          <div className="flex items-center"></div>
-        </div>
-        <div className="flex items-center justify-between mb-6">
-          <div className="flex items-center">
-            <span>
-              <span className="text-xl font-bold">8.1</span>
-              <span className="text-sm">/10</span>
-            </span>
-          </div>
-          <div className="text-sm text-gray-600">5703 оценок</div>
         </div>
         <Tabs defaultValue="about">
-          <TabsList className="flex justify-between">
-            <div className="flex">
-              <TabsTrigger
-                value="about"
-                className="px-4 py-2 text-sm font-medium"
-              >
-                О вузе
-              </TabsTrigger>
-              <TabsTrigger
-                value="reviews"
-                className="px-4 py-2 text-sm font-medium text-gray-700"
-              >
-                Отзывы
-              </TabsTrigger>
-              <TabsTrigger
-                value="open-day"
-                className="px-4 py-2 text-sm font-medium text-gray-700"
-              >
-                Дни открытых дверей
-              </TabsTrigger>
-              <TabsTrigger
-                value="dorm"
-                className="px-4 py-2 text-sm font-medium text-gray-700"
-              >
-                Общежитие
-              </TabsTrigger>
-            </div>
+          <TabsList className="flex w-full">
+            <TabsTrigger
+              value="about"
+              className="px-4 py-2 text-sm font-medium flex-1"
+            >
+              О вузе
+            </TabsTrigger>
+            <TabsTrigger
+              value="reviews"
+              className="px-4 py-2 text-sm font-medium flex-1"
+            >
+              Отзывы
+            </TabsTrigger>
+            <TabsTrigger
+              value="open-day"
+              className="px-4 py-2 text-sm font-medium flex-1"
+            >
+              Дни открытых дверей
+            </TabsTrigger>
+            <TabsTrigger
+              value="dorm"
+              className="px-4 py-2 text-sm font-medium flex-1"
+            >
+              Общежитие
+            </TabsTrigger>
           </TabsList>
           <TabsContent value="about" className="m-5">
-            <div className="grid grid-cols-3 gap-4 text-center py-4 bg-gray-200 rounded-b-lg">
+            {/*
+              <div className="grid grid-cols-3 gap-4 text-center py-4 bg-gray-200 rounded-b-lg">
               <div>
                 <div className="text-3xl font-bold">22</div>
                 <div className="text-sm">направлений подготовки</div>
@@ -96,13 +98,20 @@ export default function Page({ params }: { params: Params }) {
               Результаты представленны самим ВУЗом.
             </div>
             <div className="mt-4"></div>
+            </div>
+            </div>
+            */}
           </TabsContent>
           <TabsContent value="reviews" className="m-5">
             <h2 className="text-lg">Отзывы</h2>
             <UniversityReviews universityId={params.id} />
           </TabsContent>
           <TabsContent value="open-day" className="m-5">
-            <h2 className="text-lg">Дни открытых дверей в {universityName}</h2>
+            {isLoading ? (
+              <Skeleton className="h-6 w-1/4" />
+            ) : (
+              <h2 className="text-lg">Дни открытых дверей в {data?.name}</h2>
+            )}
             <UniversityOpenDays universityId={params.id} />
           </TabsContent>
           <TabsContent value="dorm">
