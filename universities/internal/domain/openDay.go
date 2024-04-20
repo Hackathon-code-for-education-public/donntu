c3c911304e3b32242083145600aa8ff9c1a390c6
Arteandr <artedamatik@gmail.com>
2024-04-20 19:00:31 +0300

    undo

--- universities/internal/domain/openDay.go
+++ universities/internal/domain/openDay.go
@@ -3,9 +3,9 @@ package domain
 import "time"
 
 type OpenDay struct {
-	UniversityName string    `json:"universityName"`
-	Description    string    `json:"description"`
-	Address        string    `json:"address"`
-	Link           string    `json:"link"`
-	Date           time.Time `json:"date"`
+	UniversityName string
+	Description    string
+	Address        string
+	Link           string
+	Date           time.Time
 }
