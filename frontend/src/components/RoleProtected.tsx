772ad400781fbfaaf080baa509527e3cf5f2744a
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-04-21 18:53:36 +0300

    fix

--- frontend/src/components/RoleProtected.tsx
+++ frontend/src/components/RoleProtected.tsx
@@ -5,22 +5,26 @@ export default function RoleProtected({
   requiredRoles,
 }: {
   children: React.ReactNode;
-  requiredRoles: string | string[];
+  requiredRoles?: string | string[];
 }) {
   const { user, loading } = useUser();
 
   if (loading) {
-    return (
-      <></>
-    );
+    return <></>;
   }
 
-  const rolesArray = Array.isArray(requiredRoles)
-          ? requiredRoles
-          : [requiredRoles];
+  const rolesArray =
+    requiredRoles !== null
+      ? Array.isArray(requiredRoles)
+        ? requiredRoles
+        : [requiredRoles]
+      : [];
 
-  if (!user.role || !rolesArray.includes(user.role)) {
-    return <></>
+  if (
+    !user?.role ||
+    (rolesArray.length !== 0 && !rolesArray.includes(user.role))
+  ) {
+    return <></>;
   }
 
   // Если пользователь авторизован и имеет подходящую роль, отображаем дочерние элементы
