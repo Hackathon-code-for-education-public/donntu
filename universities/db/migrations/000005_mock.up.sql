a4d66202b366b9c5c9fe9aded87ee7686fc081c2
Arteandr <artedamatik@gmail.com>
2024-04-21 22:15:06 +0300

    add mock data

---
+++ universities/db/migrations/000005_mock.up.sql
@@ -0,0 +1,29 @@
+insert into universities (id, name, long_name, logo, rating, region, study_fields, budget_places)
+values ('10', 'ДонНТУ', 'Донецкий Национальный Технический Университет',
+        'https://gas-kvas.com/grafic/uploads/posts/2024-01/gas-kvas-com-p-logotip-mgu-na-prozrachnom-fone-1.jpg', 9.9,
+        'ДНР',
+        80, 1000);
+insert into universities (id, name, long_name, logo, rating, region, study_fields, budget_places)
+values ('11', 'ИТМО', 'Университет ИТМО',
+        'https://industry3d.ru/images/data/organizations/ITMO_1636660897.png', 9.9,
+        'ДНР',
+        80, 1000);
+insert into universities (id, name, long_name, logo, rating, region, study_fields, budget_places)
+values ('12', 'ДонГУ', 'Донецкий Государственный Университет',
+        'http://donnu.ru/img/emblem.png', 9.9,
+        'ДНР',
+        80, 1000);
+insert into universities (id, name, long_name, logo, rating, region, study_fields, budget_places)
+values ('13', 'МИСИ', 'Московский государственный строительный университет',
+        'https://x-solution.ru/media/logo/channels4_profile_1.jpg', 9.9,
+        'ДНР',
+        80, 1000);
+
+insert into university_open_days (university_id, description, date, address, link)
+values ('10', 'Описание описание', '2024-01-01', 'г. Донецк', 'https://gas-kvas.com/');
+insert into university_open_days (university_id, description, date, address, link)
+values ('10', 'Описание описание', '2024-01-01', 'г. Донецк', 'https://gas-kvas.com/');
+insert into university_open_days (university_id, description, date, address, link)
+values ('12', 'Описание ', '2024-01-01', 'г. Донецк', 'https://gas-kvas.com/');
+insert into university_open_days (university_id, description, date, address, link)
+values ('12', 'Описание ', '2024-01-01', 'г. Донецк', 'https://gas-kvas.com/');
