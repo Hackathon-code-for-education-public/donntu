1f2407e1ae0f34ad2841b62ad0729d31554ba5db
Dmitry Evteev <haxrelt@gmail.com>
2024-04-21 11:38:28 +0300

    chat / 1st migration

---
+++ chat-service/migrations/000001_init.up.sql
@@ -0,0 +1,16 @@
+create table if not exists messages (
+    id bigserial NOT NULL primary key,
+    chatId text not null,
+    userId text not null,
+    text text not null
+);
+
+create table if not exists chats (
+    id text not null primary key
+);
+
+create table if not exists users_to_chats (
+    userId text not null,
+    chatId text not null references chats(id),
+    primary key (userId, chatId)
+)
